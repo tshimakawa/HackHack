32cd6974f71164642c49b326ae04ca966db2f3d3
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-02-28 16:44:22 +0900

    MovieId取得関数の作成

--- routes/getMovieID.js
+++ routes/getMovieID.js
@@ -1,7 +1,8 @@
 const mongoDB = require('mongoose');
+const router = express.Router();
 const modules = require('../public/javascripts/modules');
 
-routes.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   const movieID = modules.getMovieID.getMovieID();//MovieIDを取得
   let response = {
     movieID:movieID
