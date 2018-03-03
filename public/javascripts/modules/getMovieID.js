b69219a8c73687d5bddf8fe6b593984804148eaf
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-03-03 12:36:51 +0900

    MovieId取得関数の作成

--- public/javascripts/modules/getMovieID.js
+++ public/javascripts/modules/getMovieID.js
@@ -1,6 +1,6 @@
 const mongoDB = require('mongoose');
 //モデル（スキーマ）の宣言
-const Movie = require('../schema/movie');
+const Movie = require('./javascripts/chema/movie');
 
 exports.getMovieID = function(){
   return new Promise(function(resolve,reject){
