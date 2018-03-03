cd024e07eabb9f646987cbfb935d917a772a14b3
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-03-03 11:27:31 +0900

    MovieId取得関数の作成

--- public/javascripts/modules/getMovieID.js
+++ public/javascripts/modules/getMovieID.js
@@ -5,14 +5,15 @@ const Movie = require('../schema/movie');
 exports.getMovieID = function(){
   return new Promise(function(resolve,reject){
     console.log("enter getMovieID");
-    Movie.find({},{function(error,result){
+    Movie.find({},function(error,result){
       if(error){
+        console.log(error;);
         reject(error);
       }else{
         console.log(result);
         resolve("qag4ewos4TE");
       }
-    }});
+    });
     console.log("aaaaaaaaaaaaa");
   });
 }
