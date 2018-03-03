6840d0e700f619731501aab86bf62d050ad32d24
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-03-03 11:08:25 +0900

    MovieId取得関数の作成

--- public/javascripts/schema/movie.js
+++ public/javascripts/schema/movie.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const movie = new Schema({
-  movieID:{type:String,require:true}  
+  movieID:{type:String,require:true}
 });
