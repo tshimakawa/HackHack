d20af8c8cae1b0778055b259f3b91d4b580fbfc7
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-03-05 17:59:35 +0900

    MovieId取得関数の作成

--- routes/webhook.js
+++ routes/webhook.js
@@ -10,8 +10,16 @@ router.post('/',function(req,res){
   let text = "";
   let url = "";
   text = req.body.text;
-  url = text.substr(1,text.length-2);
-  console.log(url);
+  if(text.length > 7){//slackに投稿されたメッセージが8文字以上の場合
+    url = text.substr(1,text.length-2);
+    if(url.substr(0,6) == 'https://'){
+      console.log("aaaaaaa");
+    }else{
+      
+    }
+  }else{
+
+  }
 });
 
 module.exports = router;
