4bbccd88014fdb23b2938e74a912d4fe48e867c4
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-03-05 16:55:12 +0900

    MovieId取得関数の作成

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/',function(req,res){
-  console.log(req);
+  console.log(req.body);
 });
 
 module.exports = router;
