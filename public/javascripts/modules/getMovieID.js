e8b8a0cdcc7e702a1fe48d186007495b0014ad79
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-03-06 18:47:53 +0900

    動画関連機能の完成

--- public/javascripts/modules/getMovieID.js
+++ public/javascripts/modules/getMovieID.js
@@ -10,11 +10,12 @@ exports.getMovieID = function(){
       if(error){
         reject(error);
       }else if(mainlist_result.length == 0){//playlistが空のとき
-        console.log("aaaaaaaaaaaaaaaa");
         SubList.find({},function(error,sublist_result){
           if(error){
             reject(error);
           }else{
+            console.log("aaaaaaaaaaaaaaaa");
+            console.log(sublist_result[Math.floor(Math.random()*sublist_result.length)].movieID);
             resolve(sublist_result[Math.floor(Math.random()*sublist_result.length)].movieID);
           }
         });
